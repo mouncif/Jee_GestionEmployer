34e17a9c1ec5c147906054b2464f73713b7beda2
Mouncif <hmouncif@yahoo.fr>
2022-03-30 10:36:04 +0000

    Update EditServ.java

--- Test09032022/src/usms/fp/EditServ.java
+++ Test09032022/src/usms/fp/EditServ.java
@@ -5,7 +5,19 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+package usms.fp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Dao.DaoEmployer;
+import Dao.Employer;
 
 /**
  * Servlet implementation class EditServ
@@ -27,7 +39,21 @@ public class EditServ extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int i=0;
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String id = request.getParameter("id");
+		
+		DaoEmployer.startConnection();
+		 Employer e = DaoEmployer.find(Integer.parseInt(id));
+
+         out.print("<form action=UpdateServ method=post>");
+         out.print("<input type=hidden name=id value="+e.getId()+"><br/>");
+         out.print("Nom : <input name=name value="+e.getName()+"><br/>");
+         out.print("Password : <input name=pass value="+e.getPassword()+"><br/>");
+         out.print("Email : <input name=email value="+e.getEmail()+"><br/>");
+         out.print("Country : <input name=country value="+e.getCountry()+"><br/>");
+         out.print("<input type=submit value=Update></form>");
 	}
 
 	/**
